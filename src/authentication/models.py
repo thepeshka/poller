0b37bca256d90aebfba90b5c6eae7067466831e9
peshka <peshka5@yandex.ru>
2019-01-19 01:52:56 +0300

    fix user model add volume

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.contrib.auth.models import User as BaseUser
 
 
-class User(AbstractUser):
-    real_name = CharField(max_length=50)
+class User(BaseUser):
+    pass
