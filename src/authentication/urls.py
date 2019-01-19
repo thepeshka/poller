01f73527e60552c052718749cadccb8289c9a49d
peshka <peshka5@yandex.ru>
2019-01-19 03:11:01 +0300

    add register view

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from authentication.views import LoginView
+from authentication.views import LoginView, RegistrationView
 
 urlpatterns = [
-    path('login', LoginView.as_view())
+    path('login', LoginView.as_view()),
+    path('register', RegistrationView.as_view())
 ]
