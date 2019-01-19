0176bc3550782ea7713356d4159f8a5083f89087
peshka <peshka5@yandex.ru>
2019-01-19 03:09:19 +0300

    add register view

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login
 from django.views.generic import FormView
 
-from authentication.forms import LoginForm
+from authentication.forms import LoginForm, RegistrationForm
 
 
 class LoginView(FormView):
@@ -10,4 +10,12 @@ class LoginView(FormView):
 
     def form_valid(self, form):
         login(self.request, form.user)
+        return super().form_valid(form)
+
+
+class RegistrationView(FormView):
+    template_name = "login_view.html"
+    form_class = RegistrationForm
+
+    def form_valid(self, form):
         return super().form_valid(form)
